228fb53bad00f82820a1344b9c0307f1e83fb659
Punam Dahal <104304324+PunamDahal@users.noreply.github.com>
2024-11-24 07:24:46 +0545

    files

---
+++ sustainable-cities/script.js
@@ -0,0 +1,15 @@
+function navigateToIntro() {
+    // Redirect to the introduction page
+    window.location.href = "index.html"; // Replace with actual URL of the introduction page
+}
+
+
+function navigateToConcepts() {
+    // Redirect to the key concepts page
+    window.location.href = "key-concepts.html"; // Replace with the actual URL of the next page
+}
+
+function navigateTo(url) {
+    // Redirect to the respective sub-page
+    window.location.href = url;
+}
